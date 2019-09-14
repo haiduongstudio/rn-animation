597adefcfc6fd39ccc2d24f128723ca30468607a
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-15 03:18:12 +0700

    Create a Draggable Card with React Native Pan Responder, and Animated.decay

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Animated, PanResponder } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue = new Animated.ValueXY();
+    this._value = { x: 0, y: 0 };
+    this.animatedValue.addListener(value => (this._value = value));
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.animatedValue.setOffset({
+          x: this._value.x,
+          y: this._value.y
+        });
+        this.animatedValue.setValue({ x: 0, y: 0 });
+      },
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: this.animatedValue.x, dy: this.animatedValue.y }
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        this.animatedValue.flattenOffset();
+        Animated.decay(this.animatedValue, {
+          deceleration: 0.997,
+          velocity: { x: gestureState.vx, y: gestureState.vy }
+        }).start();
+      }
+    });
+  }
+
+  render() {
+    const animatedStyle = {
+      transform: this.animatedValue.getTranslateTransform()
+    };
+    return (
+      <View style={styles.container}>
+        <Animated.View
+          style={[styles.box, animatedStyle]}
+          {...this.panResponder.panHandlers}
+        >
+          <Text style={styles.text}> Drag Me </Text>
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +52,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
