dc8e9e26877b1c38769f7f6dedd6ebdc644dd1f9
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-15 02:31:32 +0700

    Animate Styles of a React Native View with Animated.timing

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    // this.animatedValue = new Animated.Value(1);
+    this.animatedValue = new Animated.Value(100);
+  }
+  componentDidMount() {
+    Animated.timing(this.animatedValue, {
+      // toValue: 0.3,
+      toValue: 150,
+      duration: 3000,
+      easing: Easing.bounce
+    }).start();
+  }
+
+  render() {
+    // const animatedStyle = { opacity: this.animatedValue };
+    const animatedStyle = { height: this.animatedValue };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  box: {
+    backgroundColor: '#333',
+    width: 100,
+    height: 100
+  }
 });
