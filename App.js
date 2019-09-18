b5d952b833b2754ffb6fd0057c3e1a98b2b1e35b
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-19 02:07:49 +0700

    Animate Rotation with React Native Interpolate

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.timing(this.animatedValue, {
+      toValue: 1,
+      duration: 1500
+    }).start();
+  }
+
+  render() {
+    const interpolateRotation = this.animatedValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0rad', '10rad']
+    });
+    const animatedStyle = {
+      transform: [{ rotate: interpolateRotation }]
+    };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatedStyle]}>
+          <Text style={styles.text}>Spinner</Text>
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +36,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'white'
+  }
 });
