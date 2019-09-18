852db75bab7a5c507da2423f0e42070981aae8ae
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-19 04:27:57 +0700

    Create a Flip Card Animation with React Native

--- App.js
+++ App.js
@@ -1,12 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Animated
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+    this.value = 0;
+    this.animatedValue.addListener(({ value }) => {
+      this.value = value;
+    });
+    this.frontInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg']
+    });
+    this.backInterpolate = this.animatedValue.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '360deg']
+    });
+  }
+
+  flipCard() {
+    if (this.value >= 90) {
+      // Animated.timing(this.animatedValue, {
+      //   toValue: 0,
+      //   duration: 800
+      // }).start();
+      Animated.spring(this.animatedValue, {
+        toValue: 0,
+        friction: 8,
+        tension: 10
+      }).start();
+    } else {
+      // Animated.timing(this.animatedValue, {
+      //   toValue: 180,
+      //   duration: 800
+      // }).start();
+      Animated.spring(this.animatedValue, {
+        toValue: 180,
+        friction: 8,
+        tension: 10
+      }).start();
+    }
+  }
+
+  render() {
+    const frontAnimatedStyle = {
+      transform: [{ rotateY: this.frontInterpolate }]
+    };
+    const backAnimatedStyle = {
+      transform: [{ rotateY: this.backInterpolate }]
+    };
+    return (
+      <View style={styles.container}>
+        <View>
+          <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+            <Text style={[styles.flipText]}>
+              This text is flipping on the front.
+            </Text>
+          </Animated.View>
+          <Animated.View
+            style={[styles.flipCard, styles.flipCardBack, backAnimatedStyle]}
+          >
+            <Text style={styles.flipText}>
+              This text is flipping on the back.
+            </Text>
+          </Animated.View>
+        </View>
+        <TouchableOpacity onPressOut={() => this.flipCard()}>
+          <Text>Flip!</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +84,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  flipCard: {
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+    backfaceVisibility: 'hidden'
+  },
+  flipCardBack: {
+    backgroundColor: 'red',
+    position: 'absolute',
+    top: 0
+  },
+  flipText: {
+    width: 90,
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
