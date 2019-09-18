14d5f46b537d50c2816084b25f509da1a310c3f4
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-19 01:44:59 +0700

    Animated Colors with React Native Interpolate

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.timing(this.animatedValue, {
+      toValue: 150,
+      duration: 1500
+    }).start();
+  }
+
+  render() {
+    const interpolateColor = this.animatedValue.interpolate({
+      inputRange: [0, 50],
+      outputRange: ['rgb(0, 0, 0)', 'rgb(51, 250, 170)']
+    });
+    const animatedStyle = {
+      backgroundColor: interpolateColor,
+      transform: [{ translateY: this.animatedValue }]
+    };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatedStyle]} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +35,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  box: {
+    width: 150,
+    height: 150
+  }
 });
