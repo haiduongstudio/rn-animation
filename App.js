7539e17c6d3658479b95ab3e428f62c454dc3fff
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-19 02:33:51 +0700

    Animate a sequence of React Native Animations with Animated.sequence

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue1 = new Animated.Value(0);
+    this.animatedValue2 = new Animated.Value(1);
+  }
+
+  componentDidMount() {
+    Animated.sequence([
+      Animated.timing(this.animatedValue1, {
+        toValue: 150,
+        duration: 1000
+      }),
+      Animated.spring(this.animatedValue2, {
+        toValue: 3
+      }),
+      Animated.timing(this.animatedValue1, {
+        toValue: 0,
+        duration: 1000
+      }),
+      Animated.spring(this.animatedValue2, {
+        toValue: 0.5
+      })
+    ]).start();
+  }
+
+  render() {
+    const animatedStyle = {
+      transform: [
+        { translateY: this.animatedValue1 },
+        { scale: this.animatedValue2 }
+      ]
+    };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatedStyle]}></Animated.View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +46,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#333'
+  }
 });
