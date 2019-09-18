ea09cca67b3152fa932a2127050713ea0556e5eb
Hai Nguyen Tien <haitin4bk@gmail.com>
2019-09-19 03:42:50 +0700

    Stagger React Native Animations with Animated.stagger

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Animated, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const { height } = Dimensions.get('window');
+
+export default class App extends Component {
+  componentWillMount() {
+    this.animatedValue1 = new Animated.Value(0);
+    this.animatedValue2 = new Animated.Value(0);
+    this.animatedValue3 = new Animated.Value(0);
+  }
+
+  componentDidMount() {
+    Animated.stagger(300, [
+      Animated.timing(this.animatedValue1, {
+        toValue: height,
+        duration: 1500
+      }),
+      Animated.timing(this.animatedValue2, {
+        toValue: height,
+        duration: 3000
+      }),
+      Animated.timing(this.animatedValue3, {
+        toValue: 500,
+        duration: 400
+      })
+    ]).start();
+  }
+
+  render() {
+    const animatedStyle1 = {
+      height: this.animatedValue1
+    };
+    const animatedStyle2 = {
+      height: this.animatedValue2
+    };
+    const animatedStyle3 = {
+      height: this.animatedValue3
+    };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatedStyle1]} />
+        <Animated.View style={[styles.box, animatedStyle2]} />
+        <Animated.View style={[styles.box, animatedStyle3]} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row'
   },
+  box: {
+    flex: 1,
+    backgroundColor: '#333',
+    marginHorizontal: 5
+  }
 });
